10b79142321bf81fe1720c1b2eb1e3300df79a82
Mulearg <80479884+Mulearg@users.noreply.github.com>
2024-01-19 23:09:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "/images/" + randomDiceImage;
+var randomImageSource = "images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "/images/" + randomDiceImage2;
+var randomImageSource2 = "images/" + randomDiceImage2;
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
